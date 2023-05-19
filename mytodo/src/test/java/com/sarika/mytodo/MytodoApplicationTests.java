6e937edcda6733d8c5faf9078850dec174ceba0c
Sarika Kulkarni <sarika.kulkarni05@gmail.com>
2023-05-19 15:39:27 -0400

    Initial commit

---
+++ mytodo/src/test/java/com/sarika/mytodo/MytodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sarika.mytodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MytodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
